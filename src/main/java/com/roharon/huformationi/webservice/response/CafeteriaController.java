3169a1b2a100b2cc4c1163430ad8e628aec8ce19
Aaron Roh <roharon98@gmail.com>
2020-01-05 03:00:41 +0900

    add: utterance "학식메뉴 보기"

--- src/main/java/com/roharon/huformationi/webservice/response/CafeteriaController.java
+++ src/main/java/com/roharon/huformationi/webservice/response/CafeteriaController.java
@@ -3,6 +3,7 @@ package com.roharon.huformationi.webservice.response;
 import com.google.gson.Gson;
 import com.roharon.huformationi.cafeteria.Cafeteria;
 import com.roharon.huformationi.cafeteria.CafeteriaData;
+import com.roharon.huformationi.wrapper.SkillPayload;
 import com.roharon.huformationi.wrapper.SkillResponse;
 import com.roharon.huformationi.wrapper.component.CarouselView;
 import com.roharon.huformationi.wrapper.component.SimpleTextView;
@@ -13,9 +14,7 @@ import com.roharon.huformationi.wrapper.type.QuickReply;
 import com.roharon.huformationi.wrapper.type.SkillTemplate;
 import com.roharon.huformationi.wrapper.type.buttons.shareButton;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,8 +25,15 @@ public class CafeteriaController {
 
     @ResponseBody
     @PostMapping("/cafe")
-    public SkillResponse cafe() {
+    public SkillResponse cafe(@RequestBody SkillPayload spl) {
 
+        // For test
+        if (spl.userRequest.getUtterance().contains("학식메뉴 보기")) {
+            System.out.println(spl.userRequest.getUtterance());
+            return this.ShowSeoulCafeteriaList();
+        }
+        //TODO 식당별 조회 분류
+        //타 기능 분류
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
         Date time = new Date();
 
@@ -37,7 +43,7 @@ public class CafeteriaController {
 
         String cafeResult = caf.toString();
 
-        if (cafeResult.length()==0){
+        if (cafeResult.length() == 0) {
             cafeResult = replyData.emptyCafe;
         }
 
@@ -54,9 +60,22 @@ public class CafeteriaController {
                         .build())
                 .build();
 
-        Gson gson = new Gson();
-        String result = gson.toJson(sr);
-
         return sr;
+    }
+
+    public SkillResponse ShowSeoulCafeteriaList() {
+
+        return SkillResponse.builder()
+                .template(SkillTemplate.builder()
+                        .addOutput(SimpleTextView.builder()
+                                .simpleText(SimpleText.builder()
+                                        .text("식당을 선택해주세요")
+                                        .build())
+                                .build())
+                        .addQuickReply(replyData.inmoon)
+                        .addQuickReply(replyData.gyosoo)
+                        .addQuickReply(replyData.skylounge)
+                        .build())
+                .build();
     }
 }
