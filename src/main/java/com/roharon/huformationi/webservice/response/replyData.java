6c8b6d0c7a9425a7aedf6b348c367cb54d13a9b3
Aaron Roh <roharon98@gmail.com>
2020-01-05 03:00:26 +0900

    add: Quickreply for cafeteria

--- src/main/java/com/roharon/huformationi/webservice/response/replyData.java
+++ src/main/java/com/roharon/huformationi/webservice/response/replyData.java
@@ -3,6 +3,7 @@ package com.roharon.huformationi.webservice.response;
 import com.roharon.huformationi.wrapper.SkillResponse;
 import com.roharon.huformationi.wrapper.type.QuickReply;
 import com.roharon.huformationi.wrapper.type.SkillTemplate;
+import com.sun.xml.bind.v2.model.annotation.Quick;
 
 public interface replyData {
 
@@ -23,6 +24,54 @@ public interface replyData {
     QuickReply option = QuickReply.builder()
             .label("⚙️환경설정")
             .messageText("학식메뉴")
+            .action("message")
+            .build();
+
+    QuickReply inmoon = QuickReply.builder()
+            .label("인문관")
+            .messageText("인문관 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply gyosoo = QuickReply.builder()
+            .label("교수회관")
+            .messageText("교수회관 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply skylounge = QuickReply.builder()
+            .label("스카이라운지")
+            .messageText("스카이라운지 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply gookje = QuickReply.builder()
+            .label("국제사회교육원")
+            .messageText("국제사회교육원 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply hoosenggyojik = QuickReply.builder()
+            .label("후생관 교직원식당")
+            .messageText("후생관 교직원식당 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply hoosengstudent = QuickReply.builder()
+            .label("후생관 학생식당")
+            .messageText("후생관 학생식당 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply umoon = QuickReply.builder()
+            .label("어문관")
+            .messageText("어문관 메뉴")
+            .action("message")
+            .build();
+
+    QuickReply hufsdorm = QuickReply.builder()
+            .label("기숙사식당")
+            .messageText("기숙사식당 메뉴")
             .action("message")
             .build();
 }
