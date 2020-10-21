275a44dfb754ff1f32ae3e1af7e93cd4d6b056d5
Aaron Roh <roharon98@gmail.com>
2020-10-22 00:27:11 +0900

    Modify: fit in Google checkstyle

--- src/main/java/com/roharon/huformationi/webservice/cafeteria/CafeteriaController.java
+++ src/main/java/com/roharon/huformationi/webservice/cafeteria/CafeteriaController.java
@@ -10,130 +10,144 @@ import com.roharon.huformationi.wrapper.SkillResponse;
 import com.roharon.huformationi.wrapper.component.SimpleTextView;
 import com.roharon.huformationi.wrapper.component.componentType.SimpleText;
 import com.roharon.huformationi.wrapper.type.SkillTemplate;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @AllArgsConstructor
 public class CafeteriaController {
-    private UserRepository userRepository;
+  private UserRepository userRepository;
+  Logger logger;
 
-    @ResponseBody
-    @PostMapping("/cafe")
-    public SkillResponse cafe(@RequestBody SkillPayload spl) {
+  /**
+   * Get specific cafeteria menu from hufs web
+   *
+   * @param spl
+   * SkillPayload (refer kakao i docs)
+   *
+   * @return SkillResponse
+   */
+  @ResponseBody
+  @PostMapping("/cafe")
+  public SkillResponse cafe(@RequestBody SkillPayload spl) {
 
-        if (spl.userRequest.getUtterance().contains("학식메뉴")) {
-            User usr = userRepository.findByUserKey(spl.userRequest.user.getId());
+    if (spl.userRequest.getUtterance().contains("학식메뉴")) {
+      User usr = userRepository.findByUserKey(spl.userRequest.user.getId());
 
-            if(usr == null){
-                return userData.campusChange;
-            }
-            else if(usr.getCampus().equals(User.Campus.seoul)){
-                return this.ShowSeoulCafeteriaList();
-            }
-            else if(usr.getCampus().equals(User.Campus.global)){
-                return this.ShowGlobalCafeteriaList();
-            }
-            else{
-                return replyData.homeResponse;
-            }
+      if (usr == null) {
+        return userData.campusChange;
+      } else if (usr.getCampus().equals(User.Campus.seoul)) {
+        return this.showSeoulCafeteriaList();
+      } else if (usr.getCampus().equals(User.Campus.global)) {
+        return this.showGlobalCafeteriaList();
+      } else {
+        return replyData.homeResponse;
+      }
 
-        }
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        Date time = new Date();
-
-        String nowDate = dateFormat.format(time);
-
-        Cafeteria caf;
-        CafeteriaData.Cafe cafeSelect;
-
-        switch(spl.userRequest.getUtterance()){
-            case replyData.gyosoo_text:
-                cafeSelect = Cafeteria.Cafe.GYOSOO;
-                break;
-            case replyData.inmoon_text:
-                cafeSelect = Cafeteria.Cafe.INMOON;
-                break;
-            case replyData.skylounge_text:
-                cafeSelect = Cafeteria.Cafe.SKYLOUNGE;
-                break;
-            case replyData.gookje_text:
-                cafeSelect = Cafeteria.Cafe.GOOKJE;
-                break;
-            case replyData.hoosenggyojik_text:
-                cafeSelect = Cafeteria.Cafe.HOOSENG_GYOJIK;
-                break;
-            case replyData.hoosengstudent_text:
-                cafeSelect = Cafeteria.Cafe.HOOSENG_STUDENT;
-                break;
-            case replyData.umoon_text:
-                cafeSelect = Cafeteria.Cafe.UMOON;
-                break;
-            case replyData.hufsdorm_text:
-                cafeSelect = Cafeteria.Cafe.HUFSDORM;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + spl.userRequest.getUtterance());
-        }
-
-        caf = new Cafeteria(nowDate, cafeSelect);
-        caf.processMenu();
-
-        String cafeResult = caf.toString();
-
-        if (cafeResult.length() == 0) {
-            cafeResult = replyData.emptyCafe;
-        }
-
-        return SkillResponse.builder()
-                .template(SkillTemplate.builder()
-                        .addOutput(SimpleTextView.builder()
-                                .simpleText(SimpleText.builder()
-                                        .text(cafeResult)
-                                        .build())
-                                .build())
-                        .addQuickReply(replyData.cafe)
-                        .addQuickReply(replyData.library)
-                        .addQuickReply(replyData.option)
-                        .build())
-                .build();
     }
 
-    private SkillResponse ShowSeoulCafeteriaList() {
+    String nowDate = getDate();
 
-        return SkillResponse.builder()
-                .template(SkillTemplate.builder()
-                        .addOutput(SimpleTextView.builder()
-                                .simpleText(SimpleText.builder()
-                                        .text("식당을 선택해주세요")
-                                        .build())
-                                .build())
-                        .addQuickReply(replyData.inmoon)
-                        .addQuickReply(replyData.gyosoo)
-                        .addQuickReply(replyData.skylounge)
-                        .build())
-                .build();
+    Cafeteria caf;
+    CafeteriaData.Cafe cafeSelect = null;
+
+    switch (spl.userRequest.getUtterance()) {
+      case replyData.gyosoo_text:
+        cafeSelect = Cafeteria.Cafe.GYOSOO;
+        break;
+      case replyData.inmoon_text:
+        cafeSelect = Cafeteria.Cafe.INMOON;
+        break;
+      case replyData.skylounge_text:
+        cafeSelect = Cafeteria.Cafe.SKYLOUNGE;
+        break;
+      case replyData.gookje_text:
+        cafeSelect = Cafeteria.Cafe.GOOKJE;
+        break;
+      case replyData.hoosenggyojik_text:
+        cafeSelect = Cafeteria.Cafe.HOOSENG_GYOJIK;
+        break;
+      case replyData.hoosengstudent_text:
+        cafeSelect = Cafeteria.Cafe.HOOSENG_STUDENT;
+        break;
+      case replyData.umoon_text:
+        cafeSelect = Cafeteria.Cafe.UMOON;
+        break;
+      case replyData.hufsdorm_text:
+        cafeSelect = Cafeteria.Cafe.HUFSDORM;
+        break;
+      default:
+        logger.error("Unexpected Value: " + spl.userRequest.getUtterance());
     }
 
-    private SkillResponse ShowGlobalCafeteriaList() {
+    caf = new Cafeteria(nowDate, cafeSelect);
+    caf.processMenu();
 
-        return SkillResponse.builder()
-                .template(SkillTemplate.builder()
-                        .addOutput(SimpleTextView.builder()
-                                .simpleText(SimpleText.builder()
-                                        .text("식당을 선택해주세요")
-                                        .build())
-                                .build())
-                        .addQuickReply(replyData.hoosengstudent)
-                        .addQuickReply(replyData.hoosenggyojik)
-                        .addQuickReply(replyData.hufsdorm)
-                        .addQuickReply(replyData.umoon)
-                        .addQuickReply(replyData.gookje)
-                        .build())
-                .build();
+    String cafeResult = caf.toString();
+
+    if (cafeResult.length() == 0) {
+      cafeResult = replyData.emptyCafe;
     }
+
+    return SkillResponse.builder()
+        .template(SkillTemplate.builder()
+            .addOutput(SimpleTextView.builder()
+                .simpleText(SimpleText.builder()
+                    .text(cafeResult)
+                    .build())
+                .build())
+            .addQuickReply(replyData.cafe)
+            .addQuickReply(replyData.library)
+            .addQuickReply(replyData.option)
+            .build())
+        .build();
+  }
+
+  private String getDate() {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+    Date time = new Date();
+
+    return dateFormat.format(time);
+  }
+
+  private SkillResponse showSeoulCafeteriaList() {
+
+    return SkillResponse.builder()
+        .template(SkillTemplate.builder()
+            .addOutput(SimpleTextView.builder()
+                .simpleText(SimpleText.builder()
+                    .text("식당을 선택해주세요")
+                    .build())
+                .build())
+            .addQuickReply(replyData.inmoon)
+            .addQuickReply(replyData.gyosoo)
+            .addQuickReply(replyData.skylounge)
+            .build())
+        .build();
+  }
+
+  private SkillResponse showGlobalCafeteriaList() {
+
+    return SkillResponse.builder()
+        .template(SkillTemplate.builder()
+            .addOutput(SimpleTextView.builder()
+                .simpleText(SimpleText.builder()
+                    .text("식당을 선택해주세요")
+                    .build())
+                .build())
+            .addQuickReply(replyData.hoosengstudent)
+            .addQuickReply(replyData.hoosenggyojik)
+            .addQuickReply(replyData.hufsdorm)
+            .addQuickReply(replyData.umoon)
+            .addQuickReply(replyData.gookje)
+            .build())
+        .build();
+  }
 }
